e0ada8cfb7b7062b713bba37c5fc714f366fcc8c
Oliver Caldwell <olliec87@gmail.com>
2012-08-27 23:39:46 +0100

    Added basic tests for the isObject function.

--- tests/tests.js
+++ tests/tests.js
@@ -10,5 +10,50 @@ jasmineEnv.specFilter = function(spec) {
     return htmlReporter.specFilter(spec);
 };
 
+// Configure the tests
+describe('isObject', function() {
+    it('returns true on objects', function() {
+        expect(heir.isObject({})).toBe(true);
+        var test = {};
+        expect(heir.isObject(test)).toBe(true);
+    });
+
+    it('returns true on a regex', function() {
+        expect(heir.isObject(/reg[ex]/i)).toBe(true);
+        var test = /reg[ex]/i;
+        expect(heir.isObject(test)).toBe(true);
+    });
+
+    it('returns false on arrays', function() {
+        expect(heir.isObject([])).toBe(false);
+        var test = [];
+        expect(heir.isObject(test)).toBe(false);
+    });
+
+    it('returns false on null', function() {
+        expect(heir.isObject(null)).toBe(false);
+        var test = null;
+        expect(heir.isObject(test)).toBe(false);
+    });
+
+    it('returns false on numbers', function() {
+        expect(heir.isObject(50)).toBe(false);
+        var test = 50;
+        expect(heir.isObject(test)).toBe(false);
+    });
+
+    it('returns false on strings', function() {
+        expect(heir.isObject('foo')).toBe(false);
+        var test = 'foo';
+        expect(heir.isObject(test)).toBe(false);
+    });
+
+    it('returns false on booleans', function() {
+        expect(heir.isObject(true)).toBe(false);
+        var test = true;
+        expect(heir.isObject(test)).toBe(false);
+    });
+});
+
 // Run Jasmine
 jasmineEnv.execute();
