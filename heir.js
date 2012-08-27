659619eaf29a2db1f0bd1bf17cad7c17347bd39d
Oliver Caldwell <olliec87@gmail.com>
2012-08-27 21:34:38 +0100

    Added the main source file with initialisation code. Adds the inherits method to the function prototype.

---
+++ heir.js
@@ -0,0 +1,13 @@
+/**
+ * Heir v0.0.0 - http://git.io/F87mKg
+ * Oliver Caldwell
+ * MIT license
+ */
+
+Function.prototype.inherits = function() {
+    // JSHint config - http://www.jshint.com/
+    /*jshint laxcomma:true*/
+
+    // Place the function into strict mode
+    'use strict';
+};
