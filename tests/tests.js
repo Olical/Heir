33a63092fc8129eee2325778f16aee620d7df4e6
Oliver Caldwell <olliec87@gmail.com>
2012-08-27 23:20:15 +0100

    Added the Jasmine testing suite.

---
+++ tests/tests.js
@@ -0,0 +1,14 @@
+// Set up the Jasmine environment
+var jasmineEnv = jasmine.getEnv();
+jasmineEnv.updateInterval = 1000;
+
+var htmlReporter = new jasmine.HtmlReporter();
+
+jasmineEnv.addReporter(htmlReporter);
+
+jasmineEnv.specFilter = function(spec) {
+    return htmlReporter.specFilter(spec);
+};
+
+// Run Jasmine
+jasmineEnv.execute();
