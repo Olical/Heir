e5ded75f9e1b64708114e4bf953fcc959deff972
Oliver Caldwell <olliec87@gmail.com>
2012-08-27 22:22:55 +0100

    Added the isObject and merge functions. Also added the blank clone function. All of these functions are now exposed via either the global object or AMD within a namespace object.

--- heir.js
+++ heir.js
@@ -4,12 +4,58 @@
  * MIT license
  */
 
-;(function() {
+;(function(exports) {
     // JSHint config - http://www.jshint.com/
     /*jshint laxcomma:true*/
+    /*global define:true*/
 
     // Place the script into strict mode
     'use strict';
+
+    /**
+     * Works out if a variable is a true object (created with {} etc) and not an array or anything else that usually shows up as an object.
+     *
+     * @param {Mixed} chk The variable to check to see if it is an object. It must be a pure object, not even a prototype.
+     * @return {Boolean} True if it is a true object, false if it is anything else.
+     */
+    function isObject(chk) {
+        return (chk && typeof chk === 'object' && Object.prototype.toString.call(chk) !== '[object Array]') === true;
+    }
+
+    /**
+     * Recursively merges two objects. Object `a` will be overridden by the values in object `b`.
+     * Please run the values through a cloning function first, this function does not try to clone them for you.
+     *
+     * @param {Object} a The base object to merge into.
+     * @param {Object} b The object to merge down into object `a`.
+     * @return {Object} This is object `a` but merged with `b`.
+     */
+    function merge(a, b) {
+        // Loop over all values in b. If they are not found in a then set them
+        // If both values are objects then recursively merge them
+        for(var key in b) {
+            // Make sure the value is not in __proto__ or something like that
+            if(b.hasOwnProperty(key)) {
+                // Now if a does not have it, just replace it
+                if(!a.hasOwnProperty(key)) {
+                    a[key] = b[key];
+                }
+
+                // So the base value does exist
+                // If they are both objects then merge recursively
+                else if(isObject(a[key]) && isObject(b[key])) {
+                    merge(a[key], b[key]);
+                }
+            }
+        }
+
+        // Return the merged object
+        return a;
+    }
+
+    function clone() {
+
+    }
 
     /**
      * Inherits other functions prototype objects into the current function.
@@ -22,6 +68,24 @@
         return this;
     }
 
-    // Expose the function by placing it in the Function prototype
+    // Expose the inherit function by placing it in the Function prototype
     Function.prototype.inherit = inherit;
-}());
+
+    // Create a nice little namespace to expose
+    var ns = {
+        merge: merge,
+        inherit: inherit,
+        clone: clone,
+        isObject: isObject
+    };
+
+    // And expose everything else either via AMD or a global object
+    if(typeof define === 'function' && define.amd) {
+        define(function() {
+            return ns;
+        });
+    }
+    else {
+        exports.ns = ns;
+    }
+}(this));
