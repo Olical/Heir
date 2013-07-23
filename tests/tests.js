e486a692cd13ccdefe1cd868f5ff909c2b3dd0c6
Oliver Caldwell <olliec87@gmail.com>
2013-07-23 17:54:28 +0100

    Converted the tests to my strict JSHint rules.

--- tests/tests.js
+++ tests/tests.js
@@ -1,241 +1,246 @@
-// Set up the Jasmine environment
-var jasmineEnv = jasmine.getEnv();
-jasmineEnv.updateInterval = 1000;
+(function () {
+	/*global expect,heir,jasmine,describe,it*/
+	'use strict';
 
-var htmlReporter = new jasmine.HtmlReporter();
+	// Set up the Jasmine environment
+	var jasmineEnv = jasmine.getEnv();
+	jasmineEnv.updateInterval = 1000;
 
-jasmineEnv.addReporter(htmlReporter);
+	var htmlReporter = new jasmine.HtmlReporter();
 
-jasmineEnv.specFilter = function(spec) {
-    return htmlReporter.specFilter(spec);
-};
+	jasmineEnv.addReporter(htmlReporter);
 
-// Configure the tests
-describe('heir.isObject', function() {
-    it('returns true on objects', function() {
-        expect(heir.isObject({})).toBe(true);
-        var test = {};
-        expect(heir.isObject(test)).toBe(true);
-    });
+	jasmineEnv.specFilter = function(spec) {
+		return htmlReporter.specFilter(spec);
+	};
 
-    it('returns false on a regex', function() {
-        expect(heir.isObject(/reg[ex]/i)).toBe(false);
-        var test = /reg[ex]/i;
-        expect(heir.isObject(test)).toBe(false);
-    });
+	// Configure the tests
+	describe('heir.isObject', function() {
+		it('returns true on objects', function() {
+			expect(heir.isObject({})).toBe(true);
+			var test = {};
+			expect(heir.isObject(test)).toBe(true);
+		});
 
-    it('returns false on arrays', function() {
-        expect(heir.isObject([])).toBe(false);
-        var test = [];
-        expect(heir.isObject(test)).toBe(false);
-    });
+		it('returns false on a regex', function() {
+			expect(heir.isObject(/reg[ex]/i)).toBe(false);
+			var test = /reg[ex]/i;
+			expect(heir.isObject(test)).toBe(false);
+		});
 
-    it('returns false on null', function() {
-        expect(heir.isObject(null)).toBe(false);
-        var test = null;
-        expect(heir.isObject(test)).toBe(false);
-    });
+		it('returns false on arrays', function() {
+			expect(heir.isObject([])).toBe(false);
+			var test = [];
+			expect(heir.isObject(test)).toBe(false);
+		});
 
-    it('returns false on numbers', function() {
-        expect(heir.isObject(50)).toBe(false);
-        var test = 50;
-        expect(heir.isObject(test)).toBe(false);
-    });
+		it('returns false on null', function() {
+			expect(heir.isObject(null)).toBe(false);
+			var test = null;
+			expect(heir.isObject(test)).toBe(false);
+		});
 
-    it('returns false on strings', function() {
-        expect(heir.isObject('foo')).toBe(false);
-        var test = 'foo';
-        expect(heir.isObject(test)).toBe(false);
-    });
+		it('returns false on numbers', function() {
+			expect(heir.isObject(50)).toBe(false);
+			var test = 50;
+			expect(heir.isObject(test)).toBe(false);
+		});
 
-    it('returns false on booleans', function() {
-        expect(heir.isObject(true)).toBe(false);
-        var test = true;
-        expect(heir.isObject(test)).toBe(false);
-    });
-});
+		it('returns false on strings', function() {
+			expect(heir.isObject('foo')).toBe(false);
+			var test = 'foo';
+			expect(heir.isObject(test)).toBe(false);
+		});
 
-describe('heir.merge', function() {
-    it('copies in values that did not exist', function() {
-        var chk = heir.merge({foo:true}, {bar: true});
-        expect(chk).toEqual({foo:true, bar:true});
-    });
+		it('returns false on booleans', function() {
+			expect(heir.isObject(true)).toBe(false);
+			var test = true;
+			expect(heir.isObject(test)).toBe(false);
+		});
+	});
 
-    it('copies in values that did exist', function() {
-        var chk = heir.merge({foo:true, bar:true}, {foo:false});
-        expect(chk).toEqual({foo:false, bar:true});
-    });
+	describe('heir.merge', function() {
+		it('copies in values that did not exist', function() {
+			var chk = heir.merge({foo:true}, {bar: true});
+			expect(chk).toEqual({foo:true, bar:true});
+		});
 
-    it('merges objects recursively', function() {
-        var chk = heir.merge({
-            nest: {
-                foo: true,
-                bar: true
-            }
-        }, {
-            nest: {
-                bar: false
-            }
-        });
+		it('copies in values that did exist', function() {
+			var chk = heir.merge({foo:true, bar:true}, {foo:false});
+			expect(chk).toEqual({foo:false, bar:true});
+		});
 
-        expect(chk).toEqual({
-            nest: {
-                foo: true,
-                bar: false
-            }
-        });
-    });
-});
+		it('merges objects recursively', function() {
+			var chk = heir.merge({
+				nest: {
+					foo: true,
+					bar: true
+				}
+			}, {
+				nest: {
+					bar: false
+				}
+			});
 
-describe('heir.clone', function() {
-    it('clones objects', function() {
-        var orig = {
-            foo: true,
-            bar: false
-        };
+			expect(chk).toEqual({
+				nest: {
+					foo: true,
+					bar: false
+				}
+			});
+		});
+	});
 
-        var cl = heir.clone(orig);
+	describe('heir.clone', function() {
+		it('clones objects', function() {
+			var orig = {
+				foo: true,
+				bar: false
+			};
 
-        cl.bar = true;
+			var cl = heir.clone(orig);
 
-        expect(orig).toEqual({
-            foo: true,
-            bar: false
-        });
+			cl.bar = true;
 
-        expect(cl).toEqual({
-            foo: true,
-            bar: true
-        });
-    });
+			expect(orig).toEqual({
+				foo: true,
+				bar: false
+			});
 
-    it('clones objects recursively', function() {
-        var orig = {
-            foo: true,
-            bar: false,
-            baz: {
-                one: 100,
-                two: 300
-            }
-        };
+			expect(cl).toEqual({
+				foo: true,
+				bar: true
+			});
+		});
 
-        var cl = heir.clone(orig);
+		it('clones objects recursively', function() {
+			var orig = {
+				foo: true,
+				bar: false,
+				baz: {
+					one: 100,
+					two: 300
+				}
+			};
 
-        cl.bar = true;
-        cl.baz.two = 200;
-        cl.baz.three = 300;
+			var cl = heir.clone(orig);
 
-        expect(orig).toEqual({
-            foo: true,
-            bar: false,
-            baz: {
-                one: 100,
-                two: 300
-            }
-        });
+			cl.bar = true;
+			cl.baz.two = 200;
+			cl.baz.three = 300;
 
-        expect(cl).toEqual({
-            foo: true,
-            bar: true,
-            baz: {
-                one: 100,
-                two: 200,
-                three: 300
-            }
-        });
-    });
-});
+			expect(orig).toEqual({
+				foo: true,
+				bar: false,
+				baz: {
+					one: 100,
+					two: 300
+				}
+			});
 
-describe('heir.inherit', function() {
-    it('inherits a class', function() {
-        var Base = function(){};
-        Base.prototype.foo = function() {
-            return '!foo!';
-        };
+			expect(cl).toEqual({
+				foo: true,
+				bar: true,
+				baz: {
+					one: 100,
+					two: 200,
+					three: 300
+				}
+			});
+		});
+	});
 
-        var Base2 = function(){};
-        Base2.prototype.baz = function() {
-            return '!baz!';
-        };
+	describe('heir.inherit', function() {
+		it('inherits a class', function() {
+			var Base = function(){};
+			Base.prototype.foo = function() {
+				return '!foo!';
+			};
 
-        var Sub = function(){}.inherit(Base);
-        Sub.prototype.bar = function() {
-            return '!bar!';
-        };
-        Sub.inherit(Base2);
+			var Base2 = function(){};
+			Base2.prototype.baz = function() {
+				return '!baz!';
+			};
 
-        var b = new Base();
-        var b2 = new Base2();
-        var s = new Sub();
+			var Sub = function(){}.inherit(Base);
+			Sub.prototype.bar = function() {
+				return '!bar!';
+			};
+			Sub.inherit(Base2);
 
-        expect(b.bar).not.toBeDefined();
-        expect(b2.bar).not.toBeDefined();
-        expect(b.foo()).toEqual('!foo!');
-        expect(b2.baz()).toEqual('!baz!');
-        expect(s.foo()).toEqual('!foo!');
-        expect(s.bar()).toEqual('!bar!');
-        expect(s.baz()).toEqual('!baz!');
-    });
+			var b = new Base();
+			var b2 = new Base2();
+			var s = new Sub();
 
-    it('inherits multiple classes', function() {
-        var Base = function(){};
-        Base.prototype.foo = function() {
-            return '!foo!';
-        };
+			expect(b.bar).not.toBeDefined();
+			expect(b2.bar).not.toBeDefined();
+			expect(b.foo()).toEqual('!foo!');
+			expect(b2.baz()).toEqual('!baz!');
+			expect(s.foo()).toEqual('!foo!');
+			expect(s.bar()).toEqual('!bar!');
+			expect(s.baz()).toEqual('!baz!');
+		});
 
-        var Base2 = function(){};
-        Base2.prototype.baz = function() {
-            return '!baz!';
-        };
+		it('inherits multiple classes', function() {
+			var Base = function(){};
+			Base.prototype.foo = function() {
+				return '!foo!';
+			};
 
-        var Sub = function(){}.inherit([Base, Base2]);
-        Sub.prototype.bar = function() {
-            return '!bar!';
-        };
+			var Base2 = function(){};
+			Base2.prototype.baz = function() {
+				return '!baz!';
+			};
 
-        var b = new Base();
-        var b2 = new Base2();
-        var s = new Sub();
+			var Sub = function(){}.inherit([Base, Base2]);
+			Sub.prototype.bar = function() {
+				return '!bar!';
+			};
 
-        expect(b.bar).not.toBeDefined();
-        expect(b2.bar).not.toBeDefined();
-        expect(b.foo()).toEqual('!foo!');
-        expect(b2.baz()).toEqual('!baz!');
-        expect(s.foo()).toEqual('!foo!');
-        expect(s.bar()).toEqual('!bar!');
-        expect(s.baz()).toEqual('!baz!');
-    });
+			var b = new Base();
+			var b2 = new Base2();
+			var s = new Sub();
 
-    it('does not have to be called through the prototype', function() {
-        var Base = function(){};
-        Base.prototype.foo = function() {
-            return '!foo!';
-        };
+			expect(b.bar).not.toBeDefined();
+			expect(b2.bar).not.toBeDefined();
+			expect(b.foo()).toEqual('!foo!');
+			expect(b2.baz()).toEqual('!baz!');
+			expect(s.foo()).toEqual('!foo!');
+			expect(s.bar()).toEqual('!bar!');
+			expect(s.baz()).toEqual('!baz!');
+		});
 
-        var Base2 = function(){};
-        Base2.prototype.baz = function() {
-            return '!baz!';
-        };
+		it('does not have to be called through the prototype', function() {
+			var Base = function(){};
+			Base.prototype.foo = function() {
+				return '!foo!';
+			};
 
-        var Sub = function(){};
-        heir.inherit([Base, Base2], Sub);
-        Sub.prototype.bar = function() {
-            return '!bar!';
-        };
+			var Base2 = function(){};
+			Base2.prototype.baz = function() {
+				return '!baz!';
+			};
 
-        var b = new Base();
-        var b2 = new Base2();
-        var s = new Sub();
+			var Sub = function(){};
+			heir.inherit([Base, Base2], Sub);
+			Sub.prototype.bar = function() {
+				return '!bar!';
+			};
 
-        expect(b.bar).not.toBeDefined();
-        expect(b2.bar).not.toBeDefined();
-        expect(b.foo()).toEqual('!foo!');
-        expect(b2.baz()).toEqual('!baz!');
-        expect(s.foo()).toEqual('!foo!');
-        expect(s.bar()).toEqual('!bar!');
-        expect(s.baz()).toEqual('!baz!');
-    });
-});
+			var b = new Base();
+			var b2 = new Base2();
+			var s = new Sub();
 
-// Run Jasmine
-jasmineEnv.execute();
+			expect(b.bar).not.toBeDefined();
+			expect(b2.bar).not.toBeDefined();
+			expect(b.foo()).toEqual('!foo!');
+			expect(b2.baz()).toEqual('!baz!');
+			expect(s.foo()).toEqual('!foo!');
+			expect(s.bar()).toEqual('!bar!');
+			expect(s.baz()).toEqual('!baz!');
+		});
+	});
+
+	// Run Jasmine
+	jasmineEnv.execute();
+}.call(this));
