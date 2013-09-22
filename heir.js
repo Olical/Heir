3841c251917fb2a4eb036dbe492ee4bdbcf7adc2
Oliver Caldwell <olliec87@gmail.com>
2013-09-22 12:20:19 +0100

    Added base rewrite and tests for one method.
    Need to test inherit still.

--- heir.js
+++ heir.js
@@ -8,126 +8,44 @@
 	/*global define,module*/
 	'use strict';
 
-	/**
-	 * Works out if a variable is a true object (created with {} etc) and not an array or anything else that usually shows up as an object.
-	 *
-	 * @param {Mixed} chk The variable to check to see if it is an object. It must be a pure object, not even a prototype.
-	 * @return {Boolean} True if it is a true object, false if it is anything else.
-	 */
-	function isObject(chk) {
-		return (chk && Object.prototype.toString.call(chk) === '[object Object]') === true;
-	}
+	var heir = {
+		/**
+		 * Causes your desired class to inherit from a source class. This uses
+		 * prototypical inheritance so you can override methods without ruining
+		 * the parent class.
+		 *
+		 * This will alter the actual destination class though, it does not
+		 * create a new class.
+		 *
+		 * @param {Function} destination The target class for the inheritance.
+		 * @param {Function} source Class to inherit from.
+		 */
+		inherit: function inherit(destination, source) {
+			destination.prototype = heir.createObject(source.prototype);
+			destination.prototype.constructor = destination;
+		},
 
-	/**
-	 * Recursively merges two objects. Object `a` will be overridden by the values in object `b`.
-	 * Please run the values through a cloning function first, this function does not try to clone them for you.
-	 * The base object will be edited directly, please be careful!
-	 *
-	 * @param {Object} a The base object to merge into.
-	 * @param {Object} b The object to merge down into object `a`.
-	 * @return {Object} This is object `a` but merged with `b`.
-	 */
-	function merge(a, b) {
-		// Loop over all values in b. If they are not found in a then set them
-		// If both values are objects then recursively merge them
-		for (var key in b) {
-			// Make sure the value is not in __proto__ or something like that
-			if (b.hasOwnProperty(key)) {
-				// If they are both objects then merge recursively
-				if (isObject(a[key]) && isObject(b[key])) {
-					merge(a[key], b[key]);
-				}
-
-				// Otherwise just replace the base value
-				else {
-					a[key] = b[key];
-				}
-			}
+		/**
+		 * Creates a new object with the source object nestled within its
+		 * prototype chain.
+		 *
+		 * @param {Object} source Method to insert into the new object's prototype.
+		 * @return {Object} An empty object with the source object in it's prototype chain.
+		 */
+		createObject: function createObject(source) {
+			var Host = function () {};
+			Host.prototype = source;
+			return new Host();
 		}
-
-		// Return the merged object
-		return a;
-	}
-
-	/**
-	 * Returns a recursive clone of the passed object.
-	 * So when you edit the original the clone will not change.
-	 * Used in prototypical inheritance.
-	 * It will not clone arrays.
-	 *
-	 * @param {Object} orig The original object to clone.
-	 * @return {Object} The cloned version of orig that can be edited without changing the original.
-	 */
-	function clone(orig) {
-		// Initialise variables
-		var cl = {};
-		var key;
-
-		// Loop over all values in the object
-		// If the value is an object then clone recursively
-		// Otherwise just copy the value
-		for (key in orig) {
-			if (orig.hasOwnProperty(key)) {
-				cl[key] = isObject(orig[key]) ? clone(orig[key]) : orig[key];
-			}
-		}
-
-		// Return the clone
-		return cl;
-	}
-
-	/**
-	 * Inherits other functions prototype objects into the current function.
-	 *
-	 * @param {Function|Function[]} parent A function which should have it's prototype cloned and placed into the current functions prototype. If you pass an array of functions they will all be inherited from.
-	 * @param {Function} [forceFn] Optional function to use as the current function which is inheriting the other prototypes. It will default to `this`.
-	 * @return {Function} The current function to allow chaining.
-	 */
-	function inherit(parent, forceFn) {
-		// Initialise variables
-		var fn = forceFn || this;
-		var i;
-
-		// If the parent variable is not a function then it must be an array
-		// So we have to loop over it and inherit each of them
-		// Remember to pass the current function instance!
-		if (typeof parent !== 'function') {
-			i = parent.length;
-			while (i--) {
-				inherit(parent[i], fn);
-			}
-		}
-		else {
-			// It is not an array, it is a plain function
-			// Merge it's prototype into this one
-			merge(fn.prototype, clone(parent.prototype));
-		}
-
-		// Return the current function to allow chaining
-		return fn;
-	}
-
-	// Expose the inherit function by placing it in the Function prototype
-	Function.prototype.inherit = inherit;
-
-	// Create a nice little namespace to expose
-	var ns = {
-		isObject: isObject,
-		merge: merge,
-		clone: clone,
-		inherit: inherit
 	};
 
-	// And expose everything else either via AMD or a global object
 	if (typeof define === 'function' && define.amd) {
-		define(function () {
-			return ns;
-		});
+		define(heir);
 	}
 	else if (typeof module === 'object' && module.exports) {
-		module.exports = ns;
+		module.exports = heir;
 	}
 	else {
-		this.heir = ns;
+		this.heir = heir;
 	}
 }.call(this));
--- tests/tests.js
+++ tests/tests.js
@@ -2,7 +2,6 @@
 	/*global expect,heir,jasmine,describe,it*/
 	'use strict';
 
-	// Set up the Jasmine environment
 	var jasmineEnv = jasmine.getEnv();
 	jasmineEnv.updateInterval = 1000;
 
@@ -14,233 +13,30 @@
 		return htmlReporter.specFilter(spec);
 	};
 
-	// Configure the tests
-	describe('heir.isObject', function() {
-		it('returns true on objects', function() {
-			expect(heir.isObject({})).toBe(true);
-			var test = {};
-			expect(heir.isObject(test)).toBe(true);
+	describe('heir.createObject', function () {
+		it('returns a new empty object when passed one', function () {
+			var source = {};
+			var result = heir.createObject(source);
+
+			expect(result).toEqual({});
+			expect(result).not.toBe(source);
 		});
 
-		it('returns false on a regex', function() {
-			expect(heir.isObject(/reg[ex]/i)).toBe(false);
-			var test = /reg[ex]/i;
-			expect(heir.isObject(test)).toBe(false);
-		});
+		it('inserts the source object into the prototype', function () {
+			var source = {
+				foo: true
+			};
+			var result = heir.createObject(source);
 
-		it('returns false on arrays', function() {
-			expect(heir.isObject([])).toBe(false);
-			var test = [];
-			expect(heir.isObject(test)).toBe(false);
-		});
-
-		it('returns false on null', function() {
-			expect(heir.isObject(null)).toBe(false);
-			var test = null;
-			expect(heir.isObject(test)).toBe(false);
-		});
-
-		it('returns false on numbers', function() {
-			expect(heir.isObject(50)).toBe(false);
-			var test = 50;
-			expect(heir.isObject(test)).toBe(false);
-		});
-
-		it('returns false on strings', function() {
-			expect(heir.isObject('foo')).toBe(false);
-			var test = 'foo';
-			expect(heir.isObject(test)).toBe(false);
-		});
-
-		it('returns false on booleans', function() {
-			expect(heir.isObject(true)).toBe(false);
-			var test = true;
-			expect(heir.isObject(test)).toBe(false);
+			expect(result).toEqual(source);
+			expect(result).not.toBe(source);
+			expect(result.hasOwnProperty('foo')).toBe(false);
+			expect(result.foo).toBe(true);
 		});
 	});
 
-	describe('heir.merge', function() {
-		it('copies in values that did not exist', function() {
-			var chk = heir.merge({foo:true}, {bar: true});
-			expect(chk).toEqual({foo:true, bar:true});
-		});
-
-		it('copies in values that did exist', function() {
-			var chk = heir.merge({foo:true, bar:true}, {foo:false});
-			expect(chk).toEqual({foo:false, bar:true});
-		});
-
-		it('merges objects recursively', function() {
-			var chk = heir.merge({
-				nest: {
-					foo: true,
-					bar: true
-				}
-			}, {
-				nest: {
-					bar: false
-				}
-			});
-
-			expect(chk).toEqual({
-				nest: {
-					foo: true,
-					bar: false
-				}
-			});
-		});
+	describe('heir.inherit', function () {
 	});
 
-	describe('heir.clone', function() {
-		it('clones objects', function() {
-			var orig = {
-				foo: true,
-				bar: false
-			};
-
-			var cl = heir.clone(orig);
-
-			cl.bar = true;
-
-			expect(orig).toEqual({
-				foo: true,
-				bar: false
-			});
-
-			expect(cl).toEqual({
-				foo: true,
-				bar: true
-			});
-		});
-
-		it('clones objects recursively', function() {
-			var orig = {
-				foo: true,
-				bar: false,
-				baz: {
-					one: 100,
-					two: 300
-				}
-			};
-
-			var cl = heir.clone(orig);
-
-			cl.bar = true;
-			cl.baz.two = 200;
-			cl.baz.three = 300;
-
-			expect(orig).toEqual({
-				foo: true,
-				bar: false,
-				baz: {
-					one: 100,
-					two: 300
-				}
-			});
-
-			expect(cl).toEqual({
-				foo: true,
-				bar: true,
-				baz: {
-					one: 100,
-					two: 200,
-					three: 300
-				}
-			});
-		});
-	});
-
-	describe('heir.inherit', function() {
-		it('inherits a class', function() {
-			var Base = function(){};
-			Base.prototype.foo = function() {
-				return '!foo!';
-			};
-
-			var Base2 = function(){};
-			Base2.prototype.baz = function() {
-				return '!baz!';
-			};
-
-			var Sub = function(){}.inherit(Base);
-			Sub.prototype.bar = function() {
-				return '!bar!';
-			};
-			Sub.inherit(Base2);
-
-			var b = new Base();
-			var b2 = new Base2();
-			var s = new Sub();
-
-			expect(b.bar).not.toBeDefined();
-			expect(b2.bar).not.toBeDefined();
-			expect(b.foo()).toEqual('!foo!');
-			expect(b2.baz()).toEqual('!baz!');
-			expect(s.foo()).toEqual('!foo!');
-			expect(s.bar()).toEqual('!bar!');
-			expect(s.baz()).toEqual('!baz!');
-		});
-
-		it('inherits multiple classes', function() {
-			var Base = function(){};
-			Base.prototype.foo = function() {
-				return '!foo!';
-			};
-
-			var Base2 = function(){};
-			Base2.prototype.baz = function() {
-				return '!baz!';
-			};
-
-			var Sub = function(){}.inherit([Base, Base2]);
-			Sub.prototype.bar = function() {
-				return '!bar!';
-			};
-
-			var b = new Base();
-			var b2 = new Base2();
-			var s = new Sub();
-
-			expect(b.bar).not.toBeDefined();
-			expect(b2.bar).not.toBeDefined();
-			expect(b.foo()).toEqual('!foo!');
-			expect(b2.baz()).toEqual('!baz!');
-			expect(s.foo()).toEqual('!foo!');
-			expect(s.bar()).toEqual('!bar!');
-			expect(s.baz()).toEqual('!baz!');
-		});
-
-		it('does not have to be called through the prototype', function() {
-			var Base = function(){};
-			Base.prototype.foo = function() {
-				return '!foo!';
-			};
-
-			var Base2 = function(){};
-			Base2.prototype.baz = function() {
-				return '!baz!';
-			};
-
-			var Sub = function(){};
-			heir.inherit([Base, Base2], Sub);
-			Sub.prototype.bar = function() {
-				return '!bar!';
-			};
-
-			var b = new Base();
-			var b2 = new Base2();
-			var s = new Sub();
-
-			expect(b.bar).not.toBeDefined();
-			expect(b2.bar).not.toBeDefined();
-			expect(b.foo()).toEqual('!foo!');
-			expect(b2.baz()).toEqual('!baz!');
-			expect(s.foo()).toEqual('!foo!');
-			expect(s.bar()).toEqual('!bar!');
-			expect(s.baz()).toEqual('!baz!');
-		});
-	});
-
-	// Run Jasmine
 	jasmineEnv.execute();
 }.call(this));
